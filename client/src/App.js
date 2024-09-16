8277a6d2daa5441d685445af3ec819d48ae06bee
lihiniwimalasooriya <lswimalasooriya@gmail.com>
2024-09-16 19:38:11 +0530

    add

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch('http://localhost:8080/api/users', {
+    const res = await fetch('https:/deployusersapi.vercel.app/api/users', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
