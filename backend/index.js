9085635d6a99018d0b4884c80287497adcbfeae7
lihiniwimalasooriya <lswimalasooriya@gmail.com>
2024-09-16 20:42:43 +0530

    rest

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: '',
+    origin: 'https://deployusers-frontends.vercel.app', // Replace with your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization']
   }));
 app.use(express.json());
 
