93eefde0ddd638a22d798bab29807daf78314653
lihiniwimalasooriya <lswimalasooriya@gmail.com>
2024-09-16 21:02:42 +0530

    ee

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: "", // Replace with your frontend URL
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true 
-  }));
+}));
 app.use(express.json());
 
 // MongoDB Connection
